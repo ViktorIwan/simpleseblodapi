1af2ef5cdd0409b67ee59f253a8f2400944ed4f1
Viktor <victor@doxadigital.com>
2015-05-28 01:03:47 +0700

    Compatible with Seblod 3.6.x and Joomla 3.4.1 Add FieldX Support Add Update Function

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Simpleseblodapi
 ===============
-Version: 0.1
-Last Update: 30 December 2014
+Version: 1.1
+Last Update: 28 Mei 2015
+Change Log:
+^ Compatible with Seblod 3.6.x & Joomla 3.4.1
++ Support FieldX
 
-Simple Api for SEBLOD Developer to integrate seblod with 3rd Party Component/Plugin/Module
+This is a Tweak of JCCKContent that result Simple Api for SEBLOD Developer to integrate seblod with 3rd Party Component/Plugin/Module
 
 
 # What It Is ?
@@ -18,21 +21,23 @@ Seblod is a great tools for joomla, the connection really like bread and butter.
 $content =   new simpleSeblodAPI(array(objectType));
 ```
 *objectType* is the main Object of Seblod, One of the following:
-- joomla_article // Related with main content (#__content)
-- joomla_category // Related with main category (#__category)
-- joomla_user // Related with user system (#__user)
+- 'joomla_article' // Related with main content (#__content)
+- 'joomla_category' // Related with main category (#__category)
+- 'joomla_user' // Related with user system (#__user)
 
 ## Insert Seblod Content
 ```
 $status     =   $content->create(
                  $contentTypeName,
                  $mainField,
-                 $customField 
+                 $customField,
+                 $fieldXField 
              );            
 ```
 *$contentTypeName* - String, the name of content type
 *$mainField* - Array, the field in core joomla
 *$customField* - Array, the Additional field made by seblod
+*$fieldXField* - Array, the Additional fieldX made by seblod
 
 return value - ID of new content
 
@@ -51,9 +56,49 @@ $status     =   $content->create(
                  array(
                  	'staff_level'="Manager",
                  	'staff_pic'="Albert.jpg",
+                 ),
+                 array(
+                     'dx_member_skill_x'=>array("PHP","MySQL")
                  )
              );
 ```
+note: 'dx_member_skill_x' is a field_x in a content type
+
+## Update Seblod Content
+$status     =   $content->create(
+                 $id,
+                 $mainField,
+                 $customField,
+                 $fieldXField 
+             );            
+```
+*$id* - content id (taken from #__content)
+*$mainField* - Array, the field in core joomla
+*$customField* - Array, the Additional field made by seblod
+*$fieldXField* - Array, the Additional fieldX made by seblod
+
+return value - ID of updated content
+
+Sample Usage:
+$id=11;
+$mainField=array(
+    'title'=>'Victor Doxa',
+    'state'=>'1',
+    );
+$customField=array(
+    'member_position'=>'Developer',
+    'motto'=>'Never Give Up'  
+    );
+$fieldXField=array(
+    'dx_member_skill_x'=>array("HTML/JS","Copywriting","JQuery")
+    );
+$data=$content->updateContent(
+     $id,
+     $mainField,
+     $customField,
+     $fieldXField);
+
+
 
 ## Load Seblod Content
 ```
@@ -61,9 +106,9 @@ $data=$content->load($id);
 ```
 *$id* is the ID of content
 
-return value - Content Type's object
+return value - Content Type's object with all properties
 
 
-## To Do List
-- Make the api able to render custom/json format
-- fieldX and groupX support
+## Road Plan
+- Render data in json and custom format
+- GroupX Support
--- dxseblodapi/dxseblodapi.php
+++ dxseblodapi/dxseblodapi.php
@@ -5,136 +5,339 @@ if (!$app->isSite()){
 }
 
 class simpleSeblodAPI extends JCckContent{
-	public function create( $cck, $data_content, $data_more = null )
+	
+	
+	// create
+	public function create( $cck, $data_content, $data_more = null,$data_fieldx=null )
 	{
 		if ( $this->_id ) {
 			return;
 		}
-	
+		
 		$this->_type	=	$cck;
-		$this->_instance_core	=	JTable::getInstance( $this->_object_table[$this->_object] );
-		if ( is_array( $data_more ) ) { // Note: Not All Content Type have extra table
-			$this->_instance_more	=	JCckTable::getInstance( '#__cck_store_form_'.$this->_type );
+		
+		if ( empty( $this->_object ) ) {
+			$this->_object		=	JCckDatabaseCache::loadResult( 'SELECT storage_location FROM #__cck_core_types WHERE name = "'.$this->_type.'"' );
+			$this->_columns		=	$this->_getProperties();
 		}
-	
-		$author_id 		=	0; // TODO : Get default author id
-		$parent_id		=	0;
-	
+		
+		$this->_instance_core	=	JTable::getInstance( $this->_columns['table_object'][0], $this->_columns['table_object'][1] );
+		
+		$author_id 		=	0; // TODO: Get default author id
+		$parent_id		=	0; // TODO: Get default parent_id
+		
 		// Set the author_id
-		$author_key =	$this->_columns['author'];
-	
-		if ( isset( $data_content[$author_key] ) ) {
-			$author_id	=	$data_content[$author_key];
+		if ( isset( $this->_columns['author'] ) && $this->_columns['author'] && isset( $data_content[$this->_columns['author']] ) ) {
+			$author_id	=	$data_content[$this->_columns['author']];
 		} else {
-			$user_id	=	JFactory::getUser()->get('id');
-				
+			$user_id	=	JFactory::getUser()->get( 'id' );
+			
 			if ( $user_id ) {
 				$author_id	=	$user_id;
 			}
 		}
-	
-		//Set the parent_id
-		$parent_key = $this->_columns['parent'];
-	
-		if ( isset( $data_content[$parent_key] ) ) {
-			$parent_id	=	$data_content[$parent_key];
+		
+		// Set the parent_id
+		if ( isset( $this->_columns['parent'] ) && $this->_columns['parent'] && isset( $data_content[$this->_columns['parent']] ) ) {
+			$parent_id	=	$data_content[$this->_columns['parent']];
 		}
-	
+		
+
+
+
+
 		// -------- -------- --------
 		if ( !( $this->save( 'core', $data_content ) ) ) {
 			return false;
 		}
-	
+		
 		if ( !( $this->save( 'base', array(
-				'cck'=>$this->_type,
-				'pk'=>$this->_pk,
-				'storage_location'=>$this->_object,
-				'author_id'=>$author_id,
-				'parent_id'=>$parent_id,
-				'date_time'=>JFactory::getDate()->toSql()
-		) ) ) ) {
+							'cck'=>$this->_type,
+							'pk'=>$this->_pk,
+							'storage_location'=>$this->_object,
+							'author_id'=>$author_id,
+							'parent_id'=>$parent_id,
+							'date_time'=>JFactory::getDate()->toSql()
+						 ) ) ) ) {
 			return false;
 		}
 	
-		if ( is_array( $data_more ) ) {
+		if ( is_array( $data_more ) && count( $data_more ) ) {
+			$this->_instance_more	=	JCckTable::getInstance( '#__cck_store_form_'.$this->_type );
 			$this->_instance_more->load( $this->_pk, true );
-				
+					
 			if ( !( $this->save( 'more', $data_more ) ) ) {
 				return false;
 			}
-	
+
 		}
-	
-		//TODO : Load instance info
+		
+        /*
+        Field X Processing By Default Stored in introtext
+        DX-VI
+        */
+        //Get a db connection.
+        	$db = JFactory::getDbo();
+        	$query = $db->getQuery(true);
+        	$query->select($db->quoteName(array('introtext')));
+        	$query->from($db->quoteName('#__content'));
+        	$query->where($db->quoteName('id') . '= '. $db->quote($this->_pk));
+        	$db->setQuery($query);
+        	$result = $db->loadObject();
+        	$store=$result->introtext;
+        if(is_array($data_fieldx)){
+        	//load field data
+        	foreach ($data_fieldx as $key => $value) {
+        		// Create a new query object.
+	        	$query = $db->getQuery(true);
+	        	 
+	        	// Select all records from the user profile table where key begins with "custom.".
+	        	// Order it by the ordering field.
+	        	$query->select($db->quoteName(array('extended','storage','storage_location','storage_table','storage_field')));
+	        	$query->from($db->quoteName('#__cck_core_fields'));
+	        	$query->where($db->quoteName('name') . '= '. $db->quote($key));
+	        	 
+	        	// Reset the query using our newly populated query object.
+	        	$db->setQuery($query);
+	        	 
+	        	// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+	        	$result = $db->loadObject();
+	        	$counter=0;
+	        	$store.="<br/>::".$key."::".count($value)."::/".$key."::<br/>";
+	        	
+	        	if(isset($result->extended)){
+	        		foreach ($value as $fieldXValue) {
+	        			$f_name=$result->extended;
+	        			$store	.=	'<br />::'.$f_name.'|'.$counter.'|'.$key.'::'.$fieldXValue.'::/'.$f_name.'|'.$counter.'|'.$key.'::';
+	        			$counter++;
+	        		}
+	        		$store.="<br />";
+	        	}
+	        	
+	        }
+
+        	 // Create a new query object.
+        	$query = $db->getQuery(true);
+        	
+        	// Fields to update.
+			$fields = array(
+			    $db->quoteName('introtext') . ' = ' . $db->quote($store)			    
+			);
+			 
+			// Conditions for which records should be updated.
+			$conditions = array(
+			    $db->quoteName('id') . '= '. $db->quote($this->_pk)
+			);
+			 
+			$query->update($db->quoteName('#__content'))->set($fields)->where($conditions);
+        	 
+        	// Set the query using our newly populated query object and execute it.
+        	$db->setQuery($query);
+        	$db->execute();
+
+        }
+
 		return $this->_pk;
 	}
 	
 	// load
-	public function load( $identifier, $data = true )
+	public function loadContent( $id )
 	{
-		$this->_type	=	'';
-		$this->_pk		=	'';
-		$this->_id		=	'';
-	
-		if ( is_array( $identifier ) ) {
-			$this->_object	=	$identifier[0];
-			$this->_columns	=	$this->_getProperties();
-			$this->_instance_core	=	JTable::getInstance( $this->_object_table[$this->_object] );
-				
-			if( !isset( $identifier[1] ) ) {
+		
+		$this->_columns			=	$this->_getProperties();
+	    $this->_instance_core	=	JTable::getInstance( $this->_columns['table_object'][0], $this->_columns['table_object'][1] );	
+	    if( !isset( $id) ) {
 				return;
 			}
-				
-			$base	=	JCckDatabase::loadObject( 'SELECT id, cck, pk, storage_location FROM #__cck_core WHERE storage_location = "'.(string)$identifier[0].'" AND pk = '.(int)$identifier[1] );
-		} else {
-			$base	=	JCckDatabase::loadObject( 'SELECT id, cck, pk, storage_location FROM #__cck_core WHERE id = '.(int)$identifier );
-			$this->_object	=	$base->storage_location;
-			$this->_columns	=	$this->_getProperties();
-			$this->_instance_core	=	JTable::getInstance( $this->_object_table[$this->_object] );
-		}
-	
-		$this->_type	=	$base->cck;
-		$this->_pk		=	$base->pk;
-		$this->_id		=	$base->id;
-		$this->_instance_core->load( $this->_pk );
-		$this->_instance_more		=	JCckTable::getInstance( '#__cck_store_form_'.$this->_type );
-		$this->_instance_more->load( $this->_id );
-			
+		$base					=	JCckDatabase::loadObject( 'SELECT id, cck, pk, storage_location FROM #__cck_core WHERE storage_location = "'.(string)$this->_object.'" AND pk = '.(int)$id);
+	    
+	    $this->_type				=	$base->cck;
+		$this->_pk					=	$base->pk;
+		$this->_id					=	$base->id;
+
+		
 		if ( !$this->_columns['table'] ) {
 			return;
 		}
-	
-		$this->_table	=	$this->_columns['table'];
-	
-		if ( $data === true ) {
-			$this->_properties	=	JCckDatabase::loadObject( 'SELECT a.*, b.* FROM '.$this->_table.' AS a'
-					. ' LEFT JOIN #__cck_store_form_'.$this->_type.' AS b ON b.id = a.'.$this->_columns['key']
-					. ' WHERE a.'.$this->_columns['key'].' = '.(int)$this->_pk );
-				
-		} elseif ( is_array( $data ) ) {
-			if ( isset( $data[$this->_table] ) ) {
-				$select	=	implode( ',', $data[$this->_table] );
-				unset( $data[$this->_table] );
-			} else {
-				$select	=	'*';
-			}
-			$b	=	'a';
-			$i	=	98;
-			foreach ( $data as $k=>$v ) {
-				$a		=	chr($i);
-				$select	.=	', '.$a.'.'.implode( ', '.$a.'.', $v );
-				$join	.=	' LEFT JOIN '.$k.' AS '.$a.' ON '.$a.'.id = '.$b.'.'.$this->_columns['key'];
-				$b		=	$a;
-				$i++;
-			}
-			$query	=	'SELECT a.'.$select.' FROM '.$this->_table.' AS a'
-					.	$join
-					.	' WHERE a.'.$this->_columns['key'].' = '.(int)$this->_pk;
-			$this->_properties	=	JCckDatabase::loadObject( $query );
-		}
 		
-		return $this->_properties;
+		$this->_table	=	$this->_columns['table'];
+
+		$this->_properties	=	JCckDatabase::loadObject( 'SELECT a.*, b.* FROM '.$this->_table.' AS a'
+															. ' LEFT JOIN #__cck_store_form_'.$this->_type.' AS b ON b.id = a.'.$this->_columns['key']
+															. ' WHERE a.'.$this->_columns['key'].' = '.(int)$this->_pk );
+		//look for fieldx
+		$this->_fieldx=JCckDatabase::loadObjectlist('SELECT
+		c.`name`,
+		c.type
+		FROM
+		aura_cck_core_types AS a
+		Left Join aura_cck_core_type_field As b ON b.typeid = a.id
+		Left Join aura_cck_core_fields As c ON c.id = b.fieldid
+		WHERE
+		a.`name` = \''.$base->cck.'\' AND
+		c.type = \'field_x\'
+		');
+   
+        if(is_array($this->_fieldx)){
+        	$introtext=$this->_properties->introtext;
+        	//FOUND FIELD X
+        	foreach ($this->_fieldx as $key => $value) {
+        		$field_x=$value->name;
+        		$pattern = "/::".$field_x."::(.*)::\/".$field_x."::/i";
+        		preg_match($pattern, $introtext, $matches);
+        		if(count($matches)>0){
+        		$total_array=$matches[1];
+	        		if($total_array>0){
+	        			$this->_properties->{$field_x}=array();
+		        		for($i=0;$i<$total_array;$i++){
+		        			$pattern="/::.*\|$i\|$field_x::(.*)::\/.*\|$i\|$field_x::/i";	
+		        			preg_match($pattern, $introtext, $matches);
+		        			$this->_properties->{$field_x}[]=$matches[1];
+		        		}
+	        		}
+        		}
+
+        	}
+        }
+		
+		return($this->_properties);
 	}
 
+
+	//
+	function updateContent($id,$data_content=null, $data_more = null,$data_fieldx=null ){
+		$this->_columns			=	$this->_getProperties();
+	    $this->_instance_core	=	JTable::getInstance( $this->_columns['table_object'][0], $this->_columns['table_object'][1] );	
+	    if( !isset( $id) ) {
+				return;
+			}
+		$base					=	JCckDatabase::loadObject( 'SELECT id, cck, pk, storage_location FROM #__cck_core WHERE storage_location = "'.(string)$this->_object.'" AND pk = '.(int)$id);
+	    
+	    $this->_type				=	$base->cck;
+		$this->_pk					=	$base->pk;
+		$this->_id					=	$base->id;
+
+		
+		if ( !$this->_columns['table'] ) {
+			return;
+		}
+
+	
+		$this->_table	=	$this->_columns['table'];
+		$db = JFactory::getDbo();
+
+		//MAIN
+		if(count($data_content)>0){
+			
+        	 // Create a new query object.
+        	$query = $db->getQuery(true);
+        	
+        	// Fields to update.
+        	foreach ($data_content as $key => $value) {
+        		$fields[]=$db->quoteName($key).'='.$db->quote($value);
+        	}
+
+			 
+			// Conditions for which records should be updated.
+			$conditions = array(
+			    $db->quoteName('id') . '= '. $id
+			);
+			 
+			$query->update($db->quoteName($this->_table))->set($fields)->where($conditions);
+        	 
+         	// Set the query using our newly populated query object and execute it.
+         	$db->setQuery($query);
+         	$db->execute();
+		 }
+         
+         $fields=null;
+         //EXTRA
+		 if(count($data_more)>0){
+		 	 // Create a new query object.
+        	$query = $db->getQuery(true);
+        	
+        	// Fields to update.
+        	foreach ($data_more as $key => $value) {
+        		$fields[]=$db->quoteName($key).'='.$db->quote($value);
+        	}
+
+			 
+			// Conditions for which records should be updated.
+			$conditions = array(
+			    $db->quoteName('id') . '= '. $id
+			);
+			
+			 $query->update($db->quoteName('#__cck_store_form_'.$this->_type))->set($fields)->where($conditions);
+        	 
+         	// Set the query using our newly populated query object and execute it.
+         	$db->setQuery($query);
+         	$db->execute();
+		 }
+
+		//FIELDX
+		if(is_array($data_fieldx)){
+        	//load field data
+        	$temp=	JCckDatabase::loadObject( 'SELECT a.introtext FROM '.$this->_table.' AS a'
+															. ' WHERE a.'.$this->_columns['key'].' = '.(int)$this->_pk );
+        	$introtext=$temp->introtext;
+        	
+        	foreach ($data_fieldx as $key => $value) {
+        		// Create a new query object.
+	         	$query = $db->getQuery(true);
+	        	 
+         	
+	         	$query->select($db->quoteName(array('extended','storage','storage_location','storage_table','storage_field')));
+	         	$query->from($db->quoteName('#__cck_core_fields'));
+	         	$query->where($db->quoteName('name') . '= '. $db->quote($key));
+	        	 
+	         	// Reset the query using our newly populated query object.
+	         	$db->setQuery($query);
+	        	 
+	        // 	// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+	         	$result = $db->loadObject();
+
+
+	         	$counter=0;
+	         	$newstore="::".$key."::".count($value)."::/".$key."::";
+	         	$pattern="/::".$key."::[0-9]*::\/".$key."::/i";
+	         	//Total
+	     	    $introtext=preg_replace($pattern, $newstore, $introtext);
+
+	     	    if(isset($result->extended)){
+	     	    	//Delete OldValue
+	     	    	$pattern="/::".$result->extended."\|.*".$key."::/i";
+	     	    	$introtext=preg_replace($pattern, "", $introtext);
+	     	 
+	        		foreach ($value as $fieldXValue) {
+	        			$f_name=$result->extended;
+	        			$store[]=	'<br />::'.$f_name.'|'.$counter.'|'.$key.'::'.$fieldXValue.'::/'.$f_name.'|'.$counter.'|'.$key.'::';
+	        			$counter++;
+	        		}
+	        		$store=implode("<br />", $store);
+	        		$introtext.=$store;
+	        	}
+	        }
+	        		
+        	$query = $db->getQuery(true);
+        	
+        	// Fields to update.
+			$fields = array(
+			    $db->quoteName('introtext') . ' = ' .  $db->quote($introtext)	    
+			);
+			 
+			// Conditions for which records should be updated.
+			$conditions = array(
+			    $db->quoteName('id') . '= '. $db->quote($this->_pk)
+			);
+			 
+			$query->update($db->quoteName('#__content'))->set($fields)->where($conditions);
+
+        	// Set the query using our newly populated query object and execute it.
+        	$db->setQuery($query);
+        	$db->execute();
+	        	
+	    } 
+	    return $this->_pk;
+	}
 }
 ?>
